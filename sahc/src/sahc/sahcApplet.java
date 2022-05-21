cdb409251803948bba3dff726215feb2303b8a54
MoreiraJorge <moreirajorge_17@outlook.pt>
2022-05-21 17:51:03 +0100

    Added applet code

--- sahc/src/sahc/sahcApplet.java
+++ sahc/src/sahc/sahcApplet.java
@@ -8,6 +8,13 @@ package sahc;
 
 import javacard.framework.APDU;
 import javacard.framework.Applet;
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.JCSystem;
+import javacard.security.CryptoException;
+import javacard.security.MessageDigest;
+import javacard.security.RandomData;
+import javacard.security.RandomData.OneShot;
 import javacardx.annotations.StringDef;
 import javacardx.annotations.StringPool;
 
@@ -22,7 +29,14 @@ import javacardx.annotations.StringPool;
 	    // Insert your strings here 
 	name = "sahcAppletStrings")
 public class sahcApplet extends Applet {
-
+	
+	final static byte APP_CLA = (byte) 0x80;
+	
+	final static byte INIT = (byte) 0x10;
+	
+	byte[] hash = new byte[32];
+	byte isHashEmpty = 0;
+	
     /**
      * Installs this applet.
      * 
@@ -53,6 +67,77 @@ public class sahcApplet extends Applet {
      */
     @Override
     public void process(APDU apdu) {
-        //Insert your code here
+    	byte[] buffer = apdu.getBuffer();
+        // check SELECT APDU command
+
+        if (apdu.isISOInterindustryCLA()) {
+            if (buffer[ISO7816.OFFSET_INS] == (byte) (0xA4)) {
+                return;
+            }
+            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+        }
+        
+        if (buffer[ISO7816.OFFSET_CLA] != APP_CLA) {
+            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+        }
+
+        switch (buffer[ISO7816.OFFSET_INS]) {
+            case INIT:
+                init(apdu);
+                return;
+            default:
+                ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+        }
+    }
+    
+    @SuppressWarnings("deprecation")
+	private void init(APDU apdu) {
+    	OneShot rng = null;
+    	rng = RandomData.OneShot.open(RandomData.ALG_PSEUDO_RANDOM);
+    	
+        try {
+        	
+        	if (isHashEmpty == 0) {
+        		
+        		byte[] seedBuffer = JCSystem.makeTransientByteArray((short) 32, JCSystem.CLEAR_ON_DESELECT);
+        		byte[] buffer = apdu.getBuffer();
+           	 	short le = apdu.setOutgoing();
+
+                if (le < 2) {
+                    ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+                }
+        		
+            	MessageDigest.OneShot dig = null;
+            	byte[] key = JCSystem.makeTransientByteArray((short) 8, JCSystem.CLEAR_ON_DESELECT);
+        		
+            	rng.setSeed(seedBuffer, (short) 0, (short) seedBuffer.length);
+            	rng.nextBytes(key, (short) 0, (short) key.length);
+	            
+	            JCSystem.beginTransaction();
+	            
+	            dig = MessageDigest.OneShot.open(MessageDigest.ALG_SHA_256);
+	            dig.doFinal(key, (short) 0, MessageDigest.ALG_SHA_256, hash, (short) 0);
+	            
+	            isHashEmpty = 1;
+	            
+	            JCSystem.commitTransaction();
+	            
+	            apdu.setOutgoingLength((short) key.length);
+	            
+	            buffer = key;
+	            
+	            apdu.sendBytesLong(buffer, (short) 0, (short) key.length);
+	            
+        	} else {
+        		throw new ISOException(ISO7816.SW_COMMAND_NOT_ALLOWED);
+        	}
+        	
+        } catch(CryptoException e) {
+            if (e.getReason() != CryptoException.NO_SUCH_ALGORITHM) {
+                 throw e;
+            }
+        } finally {
+        	rng.close();
+        }
     }
 }
