527b1436d6c89056b2f8a2b5492225bc7946339a
MoreiraJorge <moreirajorge_17@outlook.pt>
2022-05-24 00:40:39 +0100

    Added init with user value

--- sahc/src/sahc/sahcApplet.java
+++ sahc/src/sahc/sahcApplet.java
@@ -37,12 +37,16 @@ public class sahcApplet extends Applet {
 	
 	final static byte INIT = (byte) 0x10;
 	final static byte CIPHER = (byte) 0x11;
+	final static byte CIPHER_IV = (byte) 0x12;
 	
 	byte[] hash = new byte[32];
 	byte isHashEmpty = 1;
 	
 	DESKey desKey = (DESKey) KeyBuilder.buildKey(KeyBuilder.ALG_TYPE_DES, KeyBuilder.LENGTH_DES3_2KEY, false);
 	Cipher encryptCipher = Cipher.getInstance(Cipher.ALG_DES_CBC_ISO9797_M2, false);
+	
+	byte[] iv = new byte[8];
+	byte isIvInitialized = 0;
 	
     /**
      * Installs this applet.
@@ -94,11 +98,18 @@ public class sahcApplet extends Applet {
             case CIPHER:
             	cipherDes(apdu);
             	return;
+            case CIPHER_IV:
+            	initDesIv(apdu);
+            	return;
             default:
                 ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
         }
     }
-
+    
+    /**
+     * 
+     * @param apdu The incoming APDU
+     */
 	@SuppressWarnings("deprecation")
 	private void init(APDU apdu) {
     	RandomData rng = null;
@@ -107,23 +118,29 @@ public class sahcApplet extends Applet {
         try {
         	
         	if (isHashEmpty == 1) {
-        		
-        		byte[] seedBuffer = JCSystem.makeTransientByteArray((short) 32, JCSystem.CLEAR_ON_DESELECT);
         		byte[] buffer = apdu.getBuffer();
-           	 	short le = apdu.setOutgoing();
-
-                if (le < 2) {
-                    ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
-                }
+        		short keyMode = buffer[ISO7816.OFFSET_P1];
+        		byte[] key = null;
         		
-            	MessageDigest.OneShot dig = null;
-            	byte[] key = JCSystem.makeTransientByteArray((short) 8, JCSystem.CLEAR_ON_DESELECT);
-        		
-            	rng.setSeed(seedBuffer, (short) 0, (short) seedBuffer.length);
-            	rng.nextBytes(key, (short) 0, (short) key.length);
-	            
+            	if(keyMode == (byte)0x00) {
+            		key = JCSystem.makeTransientByteArray((short) 8, JCSystem.CLEAR_ON_DESELECT);
+	            	byte[] seedBuffer = JCSystem.makeTransientByteArray((short) 32, JCSystem.CLEAR_ON_DESELECT);
+	            	rng.setSeed(seedBuffer, (short) 0, (short) seedBuffer.length);
+	            	rng.nextBytes(key, (short) 0, (short) key.length);
+            	} else {
+            		short lc = apdu.setIncomingAndReceive();
+            		
+            		if (lc != 8) {
+                        ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+                    }
+            		
+            		key = JCSystem.makeTransientByteArray((short) lc, JCSystem.CLEAR_ON_DESELECT);
+            		Util.arrayCopyNonAtomic(buffer, ISO7816.OFFSET_CDATA, key, (short)0, (short) key.length);
+            	}
+            	
 	            JCSystem.beginTransaction();
 	            
+	            MessageDigest.OneShot dig = null;
 	            dig = MessageDigest.OneShot.open(MessageDigest.ALG_SHA_256);
 	            dig.doFinal(key, (short) 0, MessageDigest.ALG_SHA_256, hash, (short) 0);
 	            
@@ -131,11 +148,12 @@ public class sahcApplet extends Applet {
 	            
 	            JCSystem.commitTransaction();
 	            
-	            apdu.setOutgoingLength((short) key.length);
-	            
-	            buffer = key;
-	            
-	            apdu.sendBytesLong(buffer, (short) 0, (short) key.length);
+	            if(keyMode == (byte)0x00) {
+		            apdu.setOutgoing();
+		            apdu.setOutgoingLength((short) key.length);
+		            buffer = key;
+		            apdu.sendBytesLong(buffer, (short) 0, (short) key.length);
+	            }
 	            
         	} else {
         		throw new ISOException(ISO7816.SW_COMMAND_NOT_ALLOWED);
@@ -146,28 +164,31 @@ public class sahcApplet extends Applet {
                  throw e;
             }
         }
-        
     }
 	
+	/**
+	 * 
+	 * @param apdu The incoming APDU
+	 */
 	private void cipherDes(APDU apdu) {
 		try {
-			if (isHashEmpty == 0) {
+			if (isHashEmpty == 0  && isIvInitialized == 1) {
 				byte[] buffer = apdu.getBuffer();
 				short dataLen = apdu.setIncomingAndReceive();
 				byte mode = buffer[ISO7816.OFFSET_P1] == (byte)0x00 ? Cipher.MODE_ENCRYPT : Cipher.MODE_DECRYPT;
 				
 				short outgoingSize = buffer[ISO7816.OFFSET_P1] == (byte)0x00 ? getCipherSize(dataLen) : buffer[ISO7816.OFFSET_P2];
-				byte[] cipher = JCSystem.makeTransientByteArray(outgoingSize, JCSystem.CLEAR_ON_DESELECT);
+				byte[] result = JCSystem.makeTransientByteArray(outgoingSize, JCSystem.CLEAR_ON_DESELECT);
 				
 				DESKey desKey = (DESKey) KeyBuilder.buildKey(KeyBuilder.ALG_TYPE_DES, KeyBuilder.LENGTH_DES3_2KEY, false); 
 				desKey.setKey(hash, (short) 0);
 				
-				Cipher encryptCipher = Cipher.getInstance(Cipher.ALG_DES_CBC_ISO9797_M2, false);
-				encryptCipher.init(desKey, mode);
-				encryptCipher.doFinal(buffer, (short) ISO7816.OFFSET_CDATA, dataLen, cipher, (short) 0);
+				Cipher cipher = Cipher.getInstance(Cipher.ALG_DES_CBC_ISO9797_M2, false);
+				cipher.init(desKey, mode, iv, (short) 0, (short) iv.length);
+				cipher.doFinal(buffer, (short) ISO7816.OFFSET_CDATA, dataLen, result, (short) 0);
 				
-				Util.arrayCopyNonAtomic(cipher, (short) 0, buffer, ISO7816.OFFSET_CDATA, (short) cipher.length);
-				apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (short) cipher.length);
+				Util.arrayCopyNonAtomic(result, (short) 0, buffer, ISO7816.OFFSET_CDATA, (short) result.length);
+				apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (short) result.length);
 			} else {
         		throw new ISOException(ISO7816.SW_COMMAND_NOT_ALLOWED);
         	}
@@ -177,6 +198,35 @@ public class sahcApplet extends Applet {
 		}
 	}
 	
+	/**
+	 * Function to initialize DES initialization
+	 * vector with data from the incoming
+	 * APDU
+	 * @param apdu The incoming APDU
+	 */
+	private void initDesIv(APDU apdu) {
+		try {
+			byte[] buffer = apdu.getBuffer();
+			short lc = apdu.setIncomingAndReceive();
+			
+			if (lc != 8) {
+                ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+            }
+			
+			Util.arrayCopyNonAtomic(buffer, ISO7816.OFFSET_CDATA, iv, (short)0, (short) iv.length);
+			isIvInitialized = 1;
+		} catch (CryptoException e) {
+			e.getReason();
+			throw e;
+		}
+	}
+	
+	/**
+	 * Gets the size for the outgoing
+	 * result for DES in ENCRYPT mode.
+	 * The size has to match the cipher block.
+	 * @param dataLen The size of the password
+	 */
 	private short getCipherSize(short dataLen) {
 		if ((dataLen % 8) == 0) {
 			return (short) (dataLen + 8);
